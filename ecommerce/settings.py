c605a93a025b6a9c9d41750dcbd747a046836038
mrrobot2709 <akshaysingh2709@gmail.com>
2018-09-18 17:16:08 +0530

    loading static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-my-proj"),
+
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_cdn")
+
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .view import  home_page,about_page,contact_page
 
 urlpatterns = [
@@ -22,4 +24,10 @@ path('', home_page),
 path(r'about/', about_page),
 path(r'contact/', contact_page),
 path('admin/', admin.site.urls),
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
